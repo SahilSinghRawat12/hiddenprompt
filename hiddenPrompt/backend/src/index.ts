9d99387ff1b2fefa0b3331d53edaeb71fc2e63f1
Sahil Singh Rawat <sahilsrawat12@gmail.com>
2026-08-27 01:56:09 +0530

    Configure production CORS

--- hiddenPrompt/backend/src/index.ts
+++ hiddenPrompt/backend/src/index.ts
@@ -12,7 +12,7 @@ const PORT = Number(process.env.PORT) || 5000;
 const server = http.createServer(app);
 const io = new Server(server , {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://hiddenprompt.vercel.app/",
         credentials: true
     },
 });
